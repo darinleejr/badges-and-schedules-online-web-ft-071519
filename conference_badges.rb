31efc2d29829010372bdad905485cd207306a4e7
Darin Webster <darinleenavy@gmail.com>
2019-08-13 19:42:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(speaker)
   array = []
-  speaker.each_with_index do |name, index|
+  speaker.each_with_index(1) do |name, index|
   array << "Hello, #{name}! You'll be assigned to room #{index}"
   end
   array
